2c85c007ae99aa52cc3c4c7c4a5a8d0eeea844cb
hggmarks <hggmarks@gmail.com>
2022-06-29 00:12:42 -0300

    removed warnings

--- rituallog/database.py
+++ rituallog/database.py
@@ -1,13 +1,15 @@
 import warnings
 from sqlalchemy.exc import SAWarning
 from sqlmodel.sql.expression import Select, SelectOfScalar
+from sqlmodel import create_engine, Session
+from rituallog.config import settings
+from rituallog import models
+
+
 warnings.filterwarnings("ignore", category=SAWarning)
 SelectOfScalar.inherit_cache = True
 Select.inherit_cache = True
 
-from sqlmodel import create_engine, Session
-from rituallog.config import settings
-from rituallog import models
 
 engine = create_engine(settings.database.url)
 
